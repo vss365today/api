f9c53d5b294c4b7c1eea6eda5af74dec086345e1
Caleb Ely <le717@users.noreply.github.com>
2019-11-26 08:51:53 -0500

    Support protected endpoints

--- src/blueprints/__init__.py
+++ src/blueprints/__init__.py
@@ -7,25 +7,35 @@ def _factory(
     partial_module_string: str,
     url_prefix: str,
     api_version: str,
-    login_required: bool = False,
+    protected: bool = False,
     auth_function: Optional[Callable] = None
 ) -> Blueprint:
-    import_name = f"src.blueprints.{api_version}.public.{partial_module_string}"
+    # Build out the module import path
+    endpoint_folder = "protected" if protected else "public"
+    import_name = [
+        "src", "blueprints", api_version,
+        endpoint_folder, partial_module_string
+    ]
+    import_name = ".".join(import_name)
+
+    # Actually create the blueprint
     blueprint = Blueprint(
         partial_module_string,
         import_name,
         url_prefix=f"/{api_version}/{url_prefix}"
     )
 
-    # This blueprint can only be accessed via a login
-    if login_required:
-        # blueprint.before_request(login)
+    # This endpoint is not to be publicly used
+    if protected:
+        # Protected endpoints must have an authorization method
+        if auth_function is None:
+            # TODO: Raise a better exception
+            raise Exception(
+                "An authorization method must be given for protected endpoints!"  # noqa
+            )
 
-        # But only allowed people can see it
-        if auth_function is not None:
-            pass
-            # blueprint.before_request(auth_function)
-
+        # Protect the endpoint with an authorization routine
+        blueprint.before_request(auth_function)
     return blueprint
 
 
@@ -33,4 +43,4 @@ prompt = _factory("prompt", "prompt", "v1")
 search = _factory("search", "search", "v1")
 writer = _factory("writer", "writer", "v1")
 
-all_blueprints = (prompt, search, writer)
+all_blueprints = (prompt, search, writer,)
