e591a43f8c31bd7b188e71c74afa1bffbdec9bf3
Caleb Ely <le717@users.noreply.github.com>
2019-11-25 22:24:21 -0500

    Implement prompt GET

--- src/blueprints/Prompt.py
+++ src/blueprints/Prompt.py
@@ -1,5 +1,5 @@
 from flask import Blueprint
-from flask import jsonify
+from flask import abort, jsonify
 
 from webargs import fields
 from webargs.flaskparser import use_args
@@ -12,11 +12,23 @@ bp = Blueprint("prompt", __name__, url_prefix="/prompt")
 
 @bp.route("/", methods=["GET"])
 @use_args({
-    "date": fields.Date("%Y-%m-%d")
+    "date": fields.Date(
+        "%Y-%m-%d",
+        location="query",
+        required=True
+    )
 })
 def get(args: dict):
-    args["method"] = "GET"
-    return args
+    # Format the date in the proper format before fetching
+    date = args["date"].isoformat()
+
+    # If we have a prompt, return it
+    # Sweet Python 3.8+ walrus operator usage :D
+    if (prompt := database.get_prompt_by_date(date)):  # noqa
+        return jsonify(prompt)
+
+    # Default to a not found response
+    return abort(404)
 
 
 @bp.route("/", methods=["POST"])
--- src/core/database.py
+++ src/core/database.py
@@ -13,6 +13,14 @@ __all__ = [
 ]
 
 
+def __dict_factory(cursor, row):
+    """https://docs.python.org/3/library/sqlite3.html#sqlite3.Connection.row_factory"""
+    d = {}
+    for idx, col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+    return d
+
+
 def __flatten_tuple_list(tup) -> list:
     """Flatten a list of tuples into a list of actual data."""
     return [item[0] for item in tup]
@@ -22,7 +30,7 @@ def __connect_to_db() -> sqlite3.Connection:
     """Create a connection to the database."""
     config = load_app_config()
     conn = sqlite3.connect(config["DB_PATH"])
-    conn.row_factory = sqlite3.Row
+    conn.row_factory = __dict_factory
     return conn
 
 
