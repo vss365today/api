4a118f486bc01098cebbc6f6f2028bdcc8349dd3
Caleb Ely <le717@users.noreply.github.com>
2019-11-27 17:51:02 -0500

    Create make_response response

--- src/core/helpers.py
+++ src/core/helpers.py
@@ -1,7 +1,12 @@
 __all__ = [
+    "make_response",
     "make_error_response"
 ]
 
 
+def make_response(data: dict, status: int) -> tuple:
+    return (data, status)
+
+
 def make_error_response(msg: str, status: int) -> tuple:
-    return ({"error_msg": msg}, status)
+    return make_response({"error_msg": msg}, status)
