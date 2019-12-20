5b2b6acdc97fa9ebd28e17de4b1bbe16a6289417
Caleb Ely <le717@users.noreply.github.com>
2019-12-19 19:45:27 -0500

    Start impl for Writer GET

--- src/blueprints/v1/protected/writer.py
+++ src/blueprints/v1/protected/writer.py
@@ -1,23 +1,45 @@
+from flask import jsonify
+
 from webargs import fields
 from webargs.flaskparser import use_args
 
 from src.blueprints import writer
-# from src.core import database
+from src.core import database
 from src.core.helpers import make_response, make_error_response
 
 
 @writer.route("/", methods=["GET"])
 @use_args({
-    "id": fields.Str(
+    "id": fields.Str(location="query"),
+    "handle": fields.Str(location="query")
+})
+def get(args: dict):
+    if "id" in args and "handle" in args:
+        return make_error_response(
+            "Providing a Writer id and handle is not allowed!",
+            422
+        )
+
+    # TODO Get the details for a single writer
+    # result = True
+    # if result:
+    #     return make_response({}, 201)
+    return make_error_response("Unable to get details for Writer <id/handle>!", 503)
+
+
+@writer.route("/date", methods=["GET"])
+@use_args({
+    "date": fields.DateTime(
+        "%Y-%m-%d",
         location="query",
         required=True
     )
 })
-def get(args: dict):
-    # TODO Get the details for a single writer
-    result = True
-    if result:
-        return make_response({}, 201)
+def get_date(args: dict):
+    # We want the Writer for a given month
+    writer = database.get_writers_by_date(args["date"].strftime("%Y-%m"))
+    if writer:
+        return make_response(jsonify(writer), 200)
     return make_error_response("Unable to get Writer details!", 503)
 
 
