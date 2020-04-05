94e0e31bc566ff58519867f30b6ab054519aaa97
Caleb Ely <le717@users.noreply.github.com>
2020-04-05 12:10:38 -0400

    Add code to load configs

--- src/app_factory.py
+++ src/app_factory.py
@@ -1,16 +1,25 @@
 from importlib import import_module
 import json
+
 from flask import Flask
 from werkzeug.exceptions import HTTPException
 
+import src.configuration as config
 from src.blueprints import all_blueprints
 from src.extensions import init_extensions
 
 
 def create_app():
     app = Flask(__name__)
-    init_extensions(app)
 
+    # Load the app configuration
+    app.config.update(config.get_app_config("default.json"))
+    app.config.update(
+        config.get_app_config(config.get_app_config_file(app.config["ENV"]))
+    )
+
+    # Load the extensions
+    init_extensions(app)
     # Register the resources
     for bp in all_blueprints:
         import_module(bp.import_name)
