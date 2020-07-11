c92fb137ba1aebd446f6b1f021eb0d6fe79965d6
Caleb Ely <le717@users.noreply.github.com>
2020-07-11 12:52:14 -0400

    Set up save file loc/name

--- src/blueprints/v1/public/archive.py
+++ src/blueprints/v1/public/archive.py
@@ -1,33 +1,47 @@
+from datetime import datetime
 from pathlib import Path
 
 from flask import current_app
+import xlsxwriter
 
 from src.blueprints import archive
+from src.core import database
 from src.core.auth_helpers import authorize_route
 from src.core.database import archive as db_archive
-
-# from src.core.helpers import make_response, make_error_response
+from src.core.helpers import format_datetime_iso, make_response
 
 
 @authorize_route
 @archive.route("/", methods=["POST"])
 def post():
-    # spredsheet_location = Path(current_app.config["DOWNLOADS_PATH"]).resolve()
-
     # Get the full word archive
     word_archive = db_archive.get_archive()
+    archive_years = database.prompt_get_years()
+
+    # Put together the save path and file name
+    today = format_datetime_iso(datetime.now())
+    file_name = f"vss365today-word-archive-{today}.xlsx"
+    save_dir = Path(current_app.config["DOWNLOADS_PATH"]).resolve()
+    full_save_path = save_dir / file_name
+
+    # Create a new spreadsheet file
+    # with xlsxwriter.Workbook(full_save_path) as workbook:
+    #     for year in archive_years:
+    #         worksheet = workbook.add_worksheet(year)
+
+    #     worksheet.write("A1", "Hello world")
 
     # TODO Should be kicked off by finder
     # TODO Generates only once a day
     # TODO Should there be a no-dup words option?
-    # TODO Generate a new sheet for each year?
-    # TODO CSV, xlsx, JSON?
+    # TODO Generate new spreadsheet
+    # - https://xlsxwriter.readthedocs.io/getting_started.html
+    # TODO Generate a new sheet for each year
     # TODO Basic archive stats?
     # - Can these just be excel formulas?
     # TODO Generate archive generation metadata
-    # TODO Generate new spreadsheet with temp name
     # TODO Delete old spreadsheet or maybe have a 5 day backup?
     # - Doing so would add a GET option to return the latest filename
     # TODO Rename new spreadsheet to proper name
     # - What is the format of the name?
-    return {}
+    return make_response(201)
