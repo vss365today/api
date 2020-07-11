bc7ae351e176e732718a677fdb73fbab5fab5d99
Caleb Ely <le717@users.noreply.github.com>
2020-07-11 11:47:24 -0400

    Add archive blueprint (start #22)

--- src/blueprints/v1/public/archive.py
+++ src/blueprints/v1/public/archive.py
@@ -1,12 +1,19 @@
-from src.blueprints import download
+from src.blueprints import archive
+from src.core.auth_helpers import authorize_route
 
 # from src.core.helpers import make_response, make_error_response
 
 
-@download.route("/", methods=["GET"])
+@archive.route("/", methods=["GET"])
 def get():
     pass
 
     # TODO Run queries that select the data
     # TODO Should return JSON with path to generated spreadsheet
     # TODO
+
+
+@authorize_route
+@archive.route("/", methods=["POST"])
+def post():
+    pass
