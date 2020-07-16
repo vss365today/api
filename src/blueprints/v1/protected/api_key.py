e27790ee163d6951d39f08c46d0b3c6280c64104
Caleb Ely <le717@users.noreply.github.com>
2020-07-16 14:50:02 -0400

    Add post/put args

--- src/blueprints/v1/protected/api_key.py
+++ src/blueprints/v1/protected/api_key.py
@@ -1,4 +1,3 @@
-from flask import current_app
 from webargs import fields
 from webargs.flaskparser import use_args
 
@@ -9,18 +8,46 @@ from src.core import database, helpers
 @api_key.route("/", methods=["GET"])
 @use_args({"token": fields.Str()}, location="query")
 def get(args: dict):
-    # Fetch the key permissions
+    """GET request to fetch the key permissions."""
     return database.api_key.get(args["token"])
 
 
 @api_key.route("/", methods=["POST"])
-def post():
+@use_args(
+    {
+        "token": fields.Str(),
+        "desc": fields.Str(),
+        "has_admin": fields.Bool(),
+        "has_archive": fields.Bool(),
+        "has_broadcast": fields.Bool(),
+        "has_host": fields.Bool(),
+        "has_prompt": fields.Bool(),
+        "has_subscription": fields.Bool(),
+    },
+    location="json",
+)
+def post(args: dict):
+
     # database.api_key.create()
     return {}
 
 
 @api_key.route("/", methods=["PUT"])
-def put():
+@use_args(
+    {
+        "token": fields.Str(),
+        "desc": fields.Str(),
+        "has_admin": fields.Bool(),
+        "has_archive": fields.Bool(),
+        "has_broadcast": fields.Bool(),
+        "has_host": fields.Bool(),
+        "has_prompt": fields.Bool(),
+        "has_subscription": fields.Bool(),
+    },
+    location="json",
+)
+def put(args: dict):
+
     # database.api_key.update()
     return {}
 
