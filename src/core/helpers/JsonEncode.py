9ec29ebb24db4099641eb22bb05199c7a658b177
Caleb Ely <le717@users.noreply.github.com>
2020-12-25 19:00:42 -0500

    Expand custom JsonEncode to support dataclasses

--- src/core/helpers/JsonEncode.py
+++ src/core/helpers/JsonEncode.py
@@ -1,14 +1,19 @@
+import dataclasses
 from datetime import datetime
 from flask.json import JSONEncoder
 
 
 class JsonEncode(JSONEncoder):
-    """Override the default Flask JSONEncoder.
-    By default, Flask converts datetime objects to RFC 1123 dates.
-    Override this behavior to convert them to ISO 8601 format instead.
-    """
+    """Override the default Flask JSONEncoder to support more data types."""
 
     def default(self, o):
+        # Convert datetime objects to ISO 8601 vs RFC 1123
         if isinstance(o, datetime):
             return o.isoformat()
+
+        # Convert Data Classes into dictionaries
+        if dataclasses.is_dataclass(o):
+            return dataclasses.asdict(o)
+
+        # Use default encoder
         return super().default(o)
