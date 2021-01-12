41acb2eb80cab2b03fb16551ee81b2612f01d847
Caleb Ely <le717@users.noreply.github.com>
2021-01-12 11:19:12 -0500

    Impl hosting period ops into settings
    API exposure forthcoming

--- src/core/database/settings.py
+++ src/core/database/settings.py
@@ -1,9 +1,17 @@
 import json
+from datetime import datetime
 from pathlib import Path
-from typing import List
+from typing import List, Tuple
 
 
-__all__ = ["get", "update", "timings_get", "timings_update"]
+__all__ = [
+    "get",
+    "update",
+    "timings_get",
+    "timings_update",
+    "hosting_period_get",
+    "hosting_start_date_get",
+]
 
 
 def __get_path() -> Path:
@@ -45,3 +53,27 @@ def timings_update(times: List[str]) -> bool:
     content["timings"] = times
     file_path.write_text(json.dumps(content, indent=2, sort_keys=True))
     return True
+
+
+def hosting_period_get(month: int) -> Tuple[int, int]:
+    """Determine the Hosting period for the current month."""
+    # In February only, Hosts begin on the 1st and 15th
+    if month == 2:
+        return (1, 15)
+
+    # For all other months, Hosts begin on the 1st and 16th
+    return (1, 16)
+
+
+def hosting_start_date_get(today: datetime) -> int:
+    """Determine the starting date for this Hosting period."""
+    # Get the proper Hosting period
+    START_DATES = hosting_period_get(today.month)
+
+    # If the current day is between the start and (exclusive) end,
+    # we are in the first Host's period
+    if START_DATES[0] <= today.day < START_DATES[1]:
+        return START_DATES[0]
+
+    # Except it's not in that first range, so we're in the second Host's period
+    return START_DATES[1]
