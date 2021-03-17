190a72f4ed9f427483833c66a2dcd229909c9de5
Caleb Ely <le717@users.noreply.github.com>
2021-03-17 11:18:00 -0400

    Call get_connection on records.Database

--- src/core/database/core.py
+++ src/core/database/core.py
@@ -2,7 +2,7 @@ import records
 from flask import current_app
 
 
-__all__ = ["connect_to_db", "create_transaction", "flatten_records"]
+__all__ = ["connect_to_db", "flatten_records"]
 
 
 def connect_to_db() -> records.Database:
@@ -13,13 +13,7 @@ def connect_to_db() -> records.Database:
         current_app.config["DB_HOST"],
         current_app.config["DB_DBNAME"],
     )
-    conn = records.Database(conn_str)
-    return conn
-
-
-def create_transaction(db):
-    """Reach into SQLAlchemy to start a transaction."""
-    return db._engine.begin()  # skipcq: PYL-W0212
+    return records.Database(conn_str).get_connection()
 
 
 def flatten_records(tup: tuple) -> list:
