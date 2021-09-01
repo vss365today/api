8a96371084131f86c3693ab15efa17e3d4aac082
Caleb Ely <le717@users.noreply.github.com>
2021-08-31 22:21:09 -0400

    Better log names

--- run-app.sh
+++ run-app.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-gunicorn --bind 0.0.0.0:80 --workers 2 --log-level error --access-logfile /var/log/vss365today-api.gunicorn.access.log --error-logfile /var/log/vss365today-api.gunicorn.error.log wsgi:app
+gunicorn --bind 0.0.0.0:80 --workers 2 --log-level error --access-logfile /var/log/gunicorn.access.log --error-logfile /var/log/gunicorn.error.log wsgi:app
