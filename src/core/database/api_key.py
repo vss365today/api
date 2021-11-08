f99c3d73f3db8692ab12480c597e505d2266f7f0
Caleb Ely <le717@users.noreply.github.com>
2021-11-08 12:17:24 -0500

    Small ORM integration

--- src/core/database/api_key.py
+++ src/core/database/api_key.py
@@ -4,7 +4,8 @@ from typing import Dict, List, Literal, Optional
 from sqlalchemy.exc import DataError
 
 from src.core.database.core import connect_to_db
-from src.core.models.v1.ApiKey import ApiKey
+from src.core.database.models import ApiKey, db
+from src.core.models.v1.ApiKey import ApiKey as ApiKeyDC
 
 
 __all__ = ["can_access", "create", "delete", "exists", "get", "get_all", "update"]
@@ -63,32 +64,30 @@ def create(permissions: dict) -> Dict[str, str]:
 
 def delete(token: str) -> Literal[True]:
     """Delete an API key."""
-    sql = "DELETE FROM api_keys WHERE token = :token"
-    with connect_to_db() as db:
-        db.query(sql, token=token)
-        return True
+    if exists(token):
+        db.session.delete(ApiKey.query.filter_by(token=token).one())
+        db.session.commit()
+    return True
 
 
 def exists(token: str) -> bool:
     """Determine if an API key exists."""
-    sql = "SELECT 1 FROM api_keys WHERE token = :token"
-    with connect_to_db() as db:
-        return bool(db.query(sql, token=token).one())
+    return ApiKey.query.filter_by(token=token).one_or_none() is not None
 
 
-def get(token: str) -> Optional[ApiKey]:
+def get(token: str) -> Optional[ApiKeyDC]:
     """Get an API key's permissions."""
     sql = "SELECT * FROM api_keys WHERE token = :token LIMIT 1"
     with connect_to_db() as db:
         record = db.query(sql, token=token).one(as_dict=True)
-        return ApiKey(**__int_to_bool(record)) if record else None
+        return ApiKeyDC(**__int_to_bool(record)) if record else None
 
 
-def get_all() -> List[ApiKey]:
+def get_all() -> List[ApiKeyDC]:
     """Get all recorded API key's permissions."""
     sql = "SELECT * FROM api_keys"
     with connect_to_db() as db:
-        return [ApiKey(**__int_to_bool(record.as_dict())) for record in db.query(sql)]
+        return [ApiKeyDC(**__int_to_bool(record.as_dict())) for record in db.query(sql)]
 
 
 def update(permissions: dict) -> bool:
