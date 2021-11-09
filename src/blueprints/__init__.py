841ce31c7074cceae3e766d55e1889fac1597c00
Caleb Ely <le717@users.noreply.github.com>
2021-11-09 10:54:54 -0500

    Add factory method for flask_smorest
    Also remove endpoint version parameter.
    Current API is v1, flask_smorest is v2

--- src/blueprints/__init__.py
+++ src/blueprints/__init__.py
@@ -1,31 +1,30 @@
 from typing import Callable, Optional
 
 from flask import Blueprint
+from flask_smorest import Blueprint as APIBlueprint
 
 from src.core.auth_helpers import authorize_blueprint
 
 
-def _factory(
+def _api_factory(
     partial_module_string: str,
     url_prefix: str,
-    api_version: str,
     auth_function: Optional[Callable] = None,
-) -> Blueprint:
-    """Generate a blueprint registration."""
+) -> APIBlueprint:
+    """Register a flask_smorest blueprint."""
     # Build out the module import path
     endpoint_folder = "public" if auth_function is None else "protected"
     import_name = [
         "src",
-        "blueprints",
-        api_version,
+        "views",
         endpoint_folder,
         partial_module_string,
     ]
     import_path: str = ".".join(import_name)
 
     # Actually create the blueprint
-    blueprint = Blueprint(
-        partial_module_string, import_path, url_prefix=f"/{api_version}/{url_prefix}"
+    blueprint = APIBlueprint(
+        partial_module_string, import_path, url_prefix=f"/v2/{url_prefix}"
     )
 
     # Protect the endpoint with an authorization routine
@@ -35,15 +34,44 @@ def _factory(
     return blueprint
 
 
-api_key = _factory("api_key", "api-key", "v1", authorize_blueprint)
-archive = _factory("archive", "archive", "v1")
-broadcast = _factory("broadcast", "broadcast", "v1", authorize_blueprint)
-browse = _factory("browse", "browse", "v1")
-host = _factory("host", "host", "v1")
-prompt = _factory("prompt", "prompt", "v1")
-search = _factory("search", "search", "v1")
-subscription = _factory("subscription", "subscription", "v1")
-settings = _factory("settings", "settings", "v1", authorize_blueprint)
+def _factory(
+    partial_module_string: str,
+    url_prefix: str,
+    auth_function: Optional[Callable] = None,
+) -> Blueprint:
+    """Generate a blueprint registration."""
+    # Build out the module import path
+    endpoint_folder = "public" if auth_function is None else "protected"
+    import_name = [
+        "src",
+        "blueprints",
+        "v1",
+        endpoint_folder,
+        partial_module_string,
+    ]
+    import_path: str = ".".join(import_name)
+
+    # Actually create the blueprint
+    blueprint = Blueprint(
+        partial_module_string, import_path, url_prefix=f"/v1/{url_prefix}"
+    )
+
+    # Protect the endpoint with an authorization routine
+    # if one was given
+    if auth_function is not None:
+        blueprint.before_request(auth_function)
+    return blueprint
+
+
+api_key = _factory("api_key", "api-key", authorize_blueprint)
+archive = _factory("archive", "archive")
+broadcast = _factory("broadcast", "broadcast", authorize_blueprint)
+browse = _factory("browse", "browse")
+host = _factory("host", "host")
+prompt = _factory("prompt", "prompt")
+search = _factory("search", "search")
+subscription = _factory("subscription", "subscription")
+settings = _factory("settings", "settings", authorize_blueprint)
 
 all_blueprints = (
     api_key,
