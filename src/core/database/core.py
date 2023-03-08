f85c72c46e4a7c5899c8eaf89802d7d493e6ae4b
Caleb Ely <le717@users.noreply.github.com>
2023-03-08 11:05:24 -0500

    Add simple API-compatible interface to help remove outdated `records` library
    This is needed as `records` is an upgrade blocker, and I would like to
    possibly get sqlalchemy, flask, and friends pulled up to
    the latest versions without waiting for the full v2 rewrite.
    This is one of the steps needed to do that. Actual use
    of this will come later

--- src/core/database/core.py
+++ src/core/database/core.py
@@ -1,10 +1,65 @@
+from contextlib import contextmanager
+from typing import Any, Generator
+
 import records
 from flask import current_app
+from sqlalchemy.engine.result import ResultProxy, RowProxy
+from sqlalchemy.sql import text
 
 from src.configuration import get_secret
+from src.core.database.models import db
 
 
-__all__ = ["connect_to_db", "get_db_conn_uri"]
+__all__ = ["connect_to_db", "connect_to_db2", "get_db_conn_uri"]
+
+
+class _Result:
+    """Wrapper class for a raw SQL query result."""
+
+    def __init__(self, result: ResultProxy) -> None:
+        self._result: ResultProxy = result
+
+    def __iter__(self) -> Generator[dict[str, Any], None, None]:
+        """Iterate over all results from the query."""
+        for r in self.all():
+            yield r
+
+    def _to_dict(self, /, r: RowProxy) -> dict[str, Any]:
+        return dict(zip(r.keys(), r.values(), strict=True))
+
+    def all(self) -> list[dict[str, Any]]:
+        """Fetch all results from the query."""
+        return [self._to_dict(r) for r in self._result.fetchall()]
+
+    def first(self) -> dict[str, Any] | None:
+        """Fetch the first result from the query."""
+        # Specifically handle no results
+        if (r := self._result.first()) is None:
+            return None
+        return self._to_dict(r)
+
+    def one(self) -> dict[str, Any] | None:
+        """Fetch the only result from the query."""
+        # Specifically handle no results
+        if (r := self._result.fetchone()) is None:
+            return None
+        return self._to_dict(r)
+
+
+class _Query:
+    """Wrapper class for executing a SQL query."""
+
+    @staticmethod
+    def query(sql, /, **kwargs) -> _Result:
+        """Execute a SQL query."""
+        with db.engine.connect() as conn:
+            return _Result(conn.execute(text(sql), **kwargs))
+
+
+@contextmanager
+def connect_to_db2() -> Generator[type[_Query], None, None]:
+    """Create a connection to the database."""
+    yield _Query
 
 
 def get_db_conn_uri() -> str:
