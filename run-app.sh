d625be6fa1851cfed314c375b105db550eca353f
Caleb Ely <le717@users.noreply.github.com>
2023-04-02 14:31:59 -0400

    Rename file

--- run-app.sh
+++ run-app.sh
@@ -4,4 +4,4 @@ python ./scripts/make_database.py
 flask db migrate
 flask db seed
 
-gunicorn --bind 0.0.0.0:80 --workers 2 --log-level error --access-logfile /var/log/access.log --error-logfile /var/log/error.log wsgi:app
+gunicorn --bind 0.0.0.0:80 --workers 2 --log-level error --access-logfile /var/log/access.log --error-logfile /var/log/error.log app:app
