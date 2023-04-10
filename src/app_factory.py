e6d39d0cb920064b6c8b5b72def1c243259eae36
Caleb Ely <le717@users.noreply.github.com>
2023-04-10 14:35:50 -0400

    Remove v1 code

--- src/app_factory.py
+++ src/app_factory.py
@@ -1,22 +1,15 @@
-import json
 from importlib import import_module
 from os import getenv
 
 import sys_vars
 from flask import Flask
-from flask.json.provider import DefaultJSONProvider
 from flask_cors import CORS
 from flask_smorest import Api
-from werkzeug.exceptions import HTTPException
 
 import src.configuration as config
-from src.blueprints import all_blueprints, v2_blueprints
-from src.core import json_special, logger
+from src.blueprints import v2_blueprints
+from src.core import logger
 from src.core.database import models
-
-
-# Modify Flask's JSON encoder to stringify some datatypes how I want
-DefaultJSONProvider.default = staticmethod(json_special.json_output)
 
 
 def create_app() -> Flask:
@@ -50,31 +43,9 @@ def create_app() -> Flask:
     # Add a file logger to record errors
     app.logger.addHandler(logger.file_handler(app.config["LOG_PATH"]))
 
-    # Register the resources
-    for bp in all_blueprints:
-        import_module(bp.import_name)
-        app.register_blueprint(bp)
-
     # Register the v2 endpoints
     for bp in v2_blueprints:
         import_module(bp.import_name)
         api.register_blueprint(bp, name=bp.name.title())
-
-    # TODO Remove this with v1 removal
-    @app.errorhandler(HTTPException)
-    def handle_http_exception(e):
-        """Return JSON instead of HTML for API errors.
-        Copied from
-        https://flask.palletsprojects.com/en/1.1.x/errorhandling/#generic-exception-handlers
-        """
-        # Start with the correct headers and status code from the error
-        response = e.get_response()
-
-        # Replace the body with JSON
-        response.data = json.dumps(
-            {"code": e.code, "name": e.name, "description": e.description}
-        )
-        response.content_type = "application/json"
-        return response
 
     return app
--- src/core/helpers/media.py
+++ src/core/helpers/media.py
@@ -10,24 +10,12 @@ from src.configuration import get_secret
 
 
 __all__ = [
-    "delete",
     "delete_v2",
-    "download",
     "download_v2",
     "is_valid_url",
-    "move",
     "move_v2",
-    "saved_name",
     "saved_name_v2",
 ]
-
-
-def delete(prompt_id: str) -> Literal[True]:
-    """Delete any Media files attached to a Prompt."""
-    all_media = Path(get_secret("IMAGES_DIR")).glob(f"{prompt_id}*")
-    for f in all_media:
-        f.unlink()
-    return True
 
 
 def delete_v2(prompt_id: int, media_id: int | None = None) -> Literal[True]:
@@ -41,27 +29,6 @@ def delete_v2(prompt_id: int, media_id: int | None = None) -> Literal[True]:
     for f in all_media:
         f.unlink()
     return True
-
-
-def download(prompt_id: str, url: str) -> dict[str, str]:
-    """Download a Tweet's media."""
-    # Generate a random file name for the download
-    original_f_name = original_name(url)
-    temp_f_name = "{name}{ext}".format(
-        name=secrets.token_hex(12), ext=PurePath(original_f_name).suffix
-    )
-
-    # Download the media to a temp directory
-    r = requests.get(url)
-    dl_path = Path(get_secret("IMAGES_DIR_TEMP")).resolve() / temp_f_name
-    dl_path.write_bytes(r.content)
-
-    # Return the original and temp file name
-    return {
-        "original": original_f_name,
-        "temp": temp_f_name,
-        "final": saved_name(prompt_id, url),
-    }
 
 
 def download_v2(url: str) -> str:
@@ -95,14 +62,6 @@ def is_valid_url(url: str) -> bool:
         return False
 
 
-def move(details: dict) -> bool:
-    """Move a media file from the temporary directory to final location."""
-    current_path = Path(get_secret("IMAGES_DIR_TEMP")) / details["temp"]
-    final_path = Path(get_secret("IMAGES_DIR")) / details["final"]
-    current_path.replace(final_path)
-    return final_path.is_file()
-
-
 def move_v2(temp_file: str, final_file: str) -> bool:
     """Move a media file from the temporary directory to final location."""
     current_path = Path(get_secret("IMAGES_DIR_TEMP")) / temp_file
@@ -120,11 +79,6 @@ def original_name(url: str) -> str:
     if ":" in name:
         name = name[: name.find(":")]
     return name
-
-
-def saved_name(prompt_id: str, url: str) -> str:
-    """Generate the media's saved file name."""
-    return "{id}-{original}".format(id=prompt_id, original=original_name(url))
 
 
 def saved_name_v2(prompt_id: int, media_id: int, url: str) -> str:
